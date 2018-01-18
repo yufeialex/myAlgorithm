8037ce1869e3ab593f590fefe9c43584e41963a4
Yufei Xin <yufeialex@163.com>
2018-01-18 21:06:09 +0800

    中文转码，格式修改

--- src/chapter_5_stringproblem/Problem_17_LowestLexicography.java
+++ src/chapter_5_stringproblem/Problem_17_LowestLexicography.java
@@ -5,33 +5,33 @@ import java.util.Comparator;
 
 public class Problem_17_LowestLexicography {
 
-    public static class MyComparator implements Comparator<String> {
-        @Override
-        public int compare(String a, String b) {
-            return (a + b).compareTo(b + a);
-        }
-    }
+	public static class MyComparator implements Comparator<String> {
+		@Override
+		public int compare(String a, String b) {
+			return (a + b).compareTo(b + a);
+		}
+	}
 
-    public static String lowestString(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return "";
-        }
-        // �����µıȽϷ�ʽ����
-        Arrays.sort(strs, new MyComparator());
-        String res = "";
-        for (int i = 0; i < strs.length; i++) {
-            res += strs[i];
-        }
-        return res;
-    }
+	public static String lowestString(String[] strs) {
+		if (strs == null || strs.length == 0) {
+			return "";
+		}
+		// 根据新的比较方式排序
+		Arrays.sort(strs, new MyComparator());
+		String res = "";
+		for (int i = 0; i < strs.length; i++) {
+			res += strs[i];
+		}
+		return res;
+	}
 
-    public static void main(String[] args) {
-        String[] strs1 = {"jibw", "ji", "jp", "bw", "jibw"};
-        System.out.println(lowestString(strs1));
+	public static void main(String[] args) {
+		String[] strs1 = { "jibw", "ji", "jp", "bw", "jibw" };
+		System.out.println(lowestString(strs1));
 
-        String[] strs2 = {"ba", "b"};
-        System.out.println(lowestString(strs2));
+		String[] strs2 = { "ba", "b" };
+		System.out.println(lowestString(strs2));
 
-    }
+	}
 
 }
